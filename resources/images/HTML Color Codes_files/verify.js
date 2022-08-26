3f5a3f4bb256deba8b2df7f41de58c1b0e3672c3
Callum Novak <callum.j.novak@gmail.com>
2022-08-26 12:17:15 -0400

    initial commit

---
+++ resources/images/HTML Color Codes_files/verify.js
@@ -0,0 +1,10 @@
+
+
+try{__tagObject_callback_358035153853({ImpressionID:"1ad74db07b824588a03e5b1df5b5f999", ServerPublicDns:"rtbc-nyc.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "1ad74db07b824588a03e5b1df5b5f999");}catch(e){}
+try{__verify_callback_358035153853({
+ResultID:2,
+Passback:"",
+AdWidth:300,
+AdHeight:250});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "1ad74db07b824588a03e5b1df5b5f999");}catch(e){}
